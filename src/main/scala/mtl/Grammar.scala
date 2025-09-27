f4f73652bffa24716d4acbb514bfa7aab9ffbedb
amndzdzdz <zorrofx@gmx.net>
2025-09-27 07:47:33 +0200

    added simple grammar definition

---
+++ src/main/scala/mtl/Grammar.scala
@@ -0,0 +1,11 @@
+package mtl
+
+/*
+The grammar for the MTL (Mission Task Language)
+
+mission ::= {task}
+task ::= "fly to" number, number ";"
+        |"navigate to" planet ";"
+        |"measure properties"
+        |"set status"
+ */
