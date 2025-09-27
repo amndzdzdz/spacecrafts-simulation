7432717400d3d55b95753733bb9d0cd1a39ce8f5
amndzdzdz <zorrofx@gmx.net>
2025-09-27 07:56:31 +0200

    updated grammar

--- src/main/scala/mtl/Grammar.scala
+++ src/main/scala/mtl/Grammar.scala
@@ -1,11 +1,11 @@
-package mtl
-
-/*
-The grammar for the MTL (Mission Task Language)
-
-mission ::= {task}
-task ::= "fly to" number, number ";"
-        |"navigate to" planet ";"
-        |"measure properties"
-        |"set status"
- */
+//package mtl
+//
+///*
+//The grammar for the MTL (Mission Task Language)
+//
+//mission ::= {task}
+//task ::= "fly to" number, number ";"
+//        |"navigate to" planet ";"
+//        |"measure properties"
+//        |"set status" statusMessage ";"
+// */
