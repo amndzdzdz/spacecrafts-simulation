c11a9c5bc143364528c80d0a212e5710391b8fde
amndzdzdz <zorrofx@gmx.net>
2025-09-27 09:44:10 +0200

    removed Examples. import prefix

--- src/test/scala/space/Test.scala
+++ src/test/scala/space/Test.scala
@@ -2,6 +2,7 @@ package space
 
 import minitest.*
 import space.*
+import space.Examples.*
 
 // click on the green arrow in the margin to run the tests
 // <--
@@ -10,22 +11,22 @@ object Test extends SimpleTestSuite {
 
     val stops =
       List(
-        Examples.mercury,
-        Examples.venus,
-        Examples.mars,
-        Examples.jupiter,
-        Examples.saturn,
-        Examples.neptun,
-        Examples.uranus,
-        Examples.pluto,
-        Examples.earth
+        mercury,
+        venus,
+        mars,
+        jupiter,
+        saturn,
+        neptun,
+        uranus,
+        pluto,
+        earth
       )
 
     val expected =
       for (planet <- stops)
         yield Event.Waypoint(planet.position)
 
-    val mission = new Mission(Examples.solarSystem, Examples.venturer)
+    val mission = new Mission(solarSystem, venturer)
     val predicted = mission.events(Examples.tour)
 
     // check that the mission passes through one waypoint for each planet
