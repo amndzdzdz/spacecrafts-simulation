701d30115fd7bb345c4e16c4e75c9b9726b63c5a
amndzdzdz <zorrofx@gmx.net>
2025-09-27 06:57:27 +0200

    fixed missing prefix Examples

--- src/test/scala/space/Test.scala
+++ src/test/scala/space/Test.scala
@@ -8,21 +8,28 @@ import space.*
 object Test extends SimpleTestSuite {
   test("tour") {
 
-    /*
     val stops =
-      List(mercury, venus, mars, jupiter, saturn, neptun, uranus, pluto, earth)
+      List(
+        Examples.mercury,
+        Examples.venus,
+        Examples.mars,
+        Examples.jupiter,
+        Examples.saturn,
+        Examples.neptun,
+        Examples.uranus,
+        Examples.pluto,
+        Examples.earth
+      )
 
     val expected =
       for (planet <- stops)
         yield Event.Waypoint(planet.position)
 
-    val mission = new Mission(solarSystem, venturer)
-    val predicted = mission.events(tour)
+    val mission = new Mission(Examples.solarSystem, Examples.venturer)
+    val predicted = mission.events(Examples.tour)
 
     // check that the mission passes through one waypoint for each planet
     assertEquals(predicted, expected)
-    
-     */
   }
 
   test("TODO") {
