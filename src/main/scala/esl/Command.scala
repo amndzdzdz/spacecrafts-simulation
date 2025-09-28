5a68c066a40bd862e4d6640aa0dedcd1321e1da5
amndzdzdz <zorrofx@gmx.net>
2025-09-28 07:06:28 +0200

    added AST of the ETL

--- src/main/scala/esl/Command.scala
+++ src/main/scala/esl/Command.scala
@@ -1,4 +1,15 @@
 package esl
 
-// TODO: implement this type as a trait or enum
-type Command
+enum Command {
+  // This part of the AST corresponds to the task FlyTo and NavigateTo of the MTL
+  case RotateTo()
+  case Accelerate
+  case Wait(time: Double)
+  case Decelerate
+  case LogWayPoint
+  // This part of the AST corresponds to the task MeasureGravity of the MTL
+  case CalibrateSensor
+  case LogMeasurement
+  // This part of the AST corresponds to the task SetStatus of the MTL
+  case Display(text: String)
+}
